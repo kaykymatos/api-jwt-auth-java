f4e77d369692657b980d2734377660732bd0fd6c
Kayky Matos <kayky7277@gmail.com>
2025-06-08 21:36:51 -0300

    Autorização delegada para a camada de segurança.

--- src/main/java/com/kaykymatos/api/security/api_jwt_auth_java/controllers/WelcomeController.java
+++ src/main/java/com/kaykymatos/api/security/api_jwt_auth_java/controllers/WelcomeController.java
@@ -1,6 +1,5 @@
 package com.kaykymatos.api.security.api_jwt_auth_java.controllers;
 
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,12 +12,10 @@ public class WelcomeController {
     }
 
     @GetMapping("/users")
-    @PreAuthorize("hasAnyRole('MANAGERS','USERS')")
     public String users() {
         return "Authorized";
     }
     @GetMapping("/managers")
-    @PreAuthorize("hasRole('MANAGERS')")
     public String managers() {
         return "Authorized manager";
     }
--- src/main/java/com/kaykymatos/api/security/api_jwt_auth_java/security/WebSecurityConfig.java
+++ src/main/java/com/kaykymatos/api/security/api_jwt_auth_java/security/WebSecurityConfig.java
@@ -2,29 +2,33 @@ package com.kaykymatos.api.security.api_jwt_auth_java.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration   
+@Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig {
-@Bean
+    @Bean
     public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
         UserDetails user = User.withUsername("user")
-            .password(passwordEncoder.encode("123456"))
-            .roles("USERS")
-            .build();
+                .password(passwordEncoder.encode("123456"))
+                .roles("USERS")
+                .build();
 
         UserDetails admin = User.withUsername("manager")
-            .password(passwordEncoder.encode("manager"))
-            .roles("MANAGERS")
-            .build();
+                .password(passwordEncoder.encode("manager"))
+                .roles("MANAGERS")
+                .build();
 
         return new InMemoryUserDetailsManager(user, admin);
     }
@@ -34,5 +38,18 @@ public class WebSecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    
+    @Bean
+public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests((authorize) -> authorize
+            .requestMatchers(HttpMethod.GET,"/").permitAll()
+            .requestMatchers(HttpMethod.POST,"/login").permitAll() // <- ESSA LINHA É ESSENCIAL!
+            .requestMatchers(HttpMethod.GET,"/managers").hasRole("MANAGERS")
+            .requestMatchers(HttpMethod.GET,"/users").hasAnyRole("USERS", "MANAGERS")
+            .anyRequest().authenticated())
+        .formLogin(Customizer.withDefaults());
+
+    return http.build();
+}
+
+
 }
